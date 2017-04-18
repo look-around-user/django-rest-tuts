c4be543a6a2fe777f37fbc7e37744c452ceffe24
DevRo5000 <per.regy@gmail.com>
2017-04-18 23:10:53 +0000

    end tut 4

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^rest/', include('rest_framework.urls', namespace='rest_framework')),
+]
