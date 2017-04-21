f48dfb2167aa27f3b4e6455b939c576969a1ef0e
DevRo5000 <per.regy@gmail.com>
2017-04-21 09:05:39 +0000

    end of tut 5

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Snippet
 from .serializers import SnippetSerializer
 from .serializers import UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+
+@api_view
+def api_root(request, format=None):
+    return Response({
+                    'user': reverse('user-list', request=request, format=format),
+                    'snippets': reverse('snippet-list', request=request, format=format)
+                    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -30,3 +42,12 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHightlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
